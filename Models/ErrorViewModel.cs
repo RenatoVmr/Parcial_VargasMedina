d9d9e364a54cb29f4f93974ba4c6e1ea97cc0aa9
RenatoVmr <Renato_Vargas2@usmp.pe>
2025-10-04 14:42:38 -0500

    Initial commit - Parcial VargasMedina project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Parcial_VargasMedina.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
